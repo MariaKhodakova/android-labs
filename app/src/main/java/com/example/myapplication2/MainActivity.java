6b8905f7028f7bab3c6dbcdfea7570af01941099
MariaKhodakova <hodakovamarija@yandex.ru>
2022-12-19 16:50:32 +0300

    df

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.myapplication2;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-public class MainActivity extends AppCompatActivity {
+    EditText email;
+    EditText password;
+    Button loginbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email=findViewById(R.id.emailid);
+        password=findViewById(R.id.editTextTextPassword2);
+        loginbutton = findViewById(R.id.button2);
+        loginbutton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button2:
+                String e = email.getText().toString();
+                String p = password.getText().toString();
+                if ((e.equals("email.com"))&(p.equals("123"))){
+                    Intent i = new Intent(this, MyArrayList.class);
+                    i.putExtra("e", e);
+                    startActivity(i);
+                }else  {
+                    Toast.makeText(MainActivity.this, "Пароль и почта введены с ошибкой!", Toast.LENGTH_LONG).show();
+                    email.setText("");
+                    password.setText("");
+                }
+
+                break;
+            default:
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/example/myapplication2/MyArrayList.java
+++ app/src/main/java/com/example/myapplication2/MyArrayList.java
@@ -1,39 +1,46 @@
 package com.example.myapplication2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import java.util.*;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MyArrayList extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MyArrayList extends AppCompatActivity implements View.OnClickListener{
 
     EditText edittext1;
-    Button Button,delete;
+    TextView textView1;
+    Button Button, exite;
     ListView textList;
     ArrayList<String> myStringArray;
     ArrayAdapter<String> TextAdapter;
-    String item;
-    Integer indecxVal;
 
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.array_list_activity);
 
         Button = findViewById(R.id.button1);
-        delete=findViewById(R.id.button);
         edittext1 = findViewById(R.id.editTextTextPersonName);
+        textView1 = (TextView)findViewById(R.id.textView);
         textList = findViewById(R.id.textList);
         myStringArray = new ArrayList<String>();
         TextAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myStringArray);
+
+        exite = findViewById(R.id.button);
+        exite.setOnClickListener(this);
+
+        String email = getIntent().getStringExtra("e");
+        textView1.setText(textView1.getText().toString() + " " + email);
 
         Button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,32 +49,24 @@ public class MyArrayList extends AppCompatActivity {
                 myStringArray.add(e);
                 textList.setAdapter(TextAdapter);
                 TextAdapter.notifyDataSetChanged();
-            }
-         });
-
-        //select item
-        textList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                item = adapterView.getItemAtPosition(i).toString()+"has been selected";
-                indecxVal = i;
-                Toast.makeText(MyArrayList.this, item, Toast.LENGTH_LONG).show();
-            }
-        });
-        //кнопка очистить
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String e = edittext1.getText().toString();
-                myStringArray.clear();
-                textList.setAdapter(TextAdapter);
-                TextAdapter.notifyDataSetChanged();
-                myStringArray.remove(indecxVal);
-                textList.setAdapter(TextAdapter);
-                TextAdapter.notifyDataSetChanged();
+                edittext1.setText("");
             }
         });
 
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+
+                break;
+            default:
+                break;
+        }
     }
 
 }
